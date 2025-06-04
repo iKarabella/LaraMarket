76e3883355047f3764f3d4e7da3fd191a4e4fe08
Konstantin Gordeev <zlaya@younan.ru>
2025-06-04 13:57:16 +0000

    все на ноль

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Laramarket">
+    </Head>
+    <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+        asdf
+    </div>
+</template>
